0f3778be4f4bb68ab25b639cae528a671f65946f
Matheus <matheus.progra.dev@gmail.com>
2022-05-22 01:34:26 -0300

    Front end Todo criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { UpdateComponent } from './components/update/update.component';
+import { CreateComponent } from './components/create/create.component';
+import { FinalizadosComponent } from './components/finalizados/finalizados.component';
+import { ReadAllComponent } from './components/read-all/read-all.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ReadAllComponent
+  },
+  {
+    path: 'finalizados',
+    component: FinalizadosComponent
+  },
+  {
+    path: 'create',
+    component: CreateComponent
+  },
+  {
+    path: 'update/:id',
+    component: UpdateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
